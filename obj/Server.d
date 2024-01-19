6ec5002e5436508234b425c35f731897daf37efc
Deyan Dyankov <ddyankov@c4r1p3.42vienna.com>
2024-01-19 14:48:24 +0100

    begin

--- obj/Server.d
+++
@@ -1,6 +0,0 @@
-obj/Server.o: srcs/Server.cpp srcs/../includes/Server.hpp \
-  srcs/../includes/Lib.hpp
-
-srcs/../includes/Server.hpp:
-
-srcs/../includes/Lib.hpp:
--- obj/main.d
+++
@@ -1,6 +0,0 @@
-obj/main.o: srcs/main.cpp srcs/../includes/Server.hpp \
-  srcs/../includes/Lib.hpp
-
-srcs/../includes/Server.hpp:
-
-srcs/../includes/Lib.hpp:
